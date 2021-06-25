d286a35befba2c7d1901823dbebf6786ae11b4a3
swbvelasquez <swbvelasquez@gmail.com>
2021-06-24 20:04:03 -0500

    Form 2 injection

--- app/src/main/java/com/tutoriales/simpledagger2/MainActivity.java
+++ app/src/main/java/com/tutoriales/simpledagger2/MainActivity.java
@@ -8,16 +8,31 @@ import com.tutoriales.simpledagger2.components.CarComponent;
 import com.tutoriales.simpledagger2.components.DaggerCarComponent;
 import com.tutoriales.simpledagger2.entities.Car;
 
-public class MainActivity extends AppCompatActivity {
-    private Car car;
+import javax.inject.Inject;
 
+public class MainActivity extends AppCompatActivity {
+
+    @Inject
+    Car car2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Detras estan las clases injectadas y el componente que se auto construye con la clase component en la interfaz declarada
-        CarComponent carComponent = DaggerCarComponent.create();
+        injectionForm2();
+    }
+
+    private void injectionForm1(){
+        //injeccion 1 a 1
+        Car car;
+        CarComponent carComponent = DaggerCarComponent.create();  //Detras estan las clases injectadas y el componente que se auto construye con la clase component en la interfaz declarada
         car = carComponent.getCar();
         car.drive();
+    }
+
+    private void injectionForm2(){
+        //injeccion por parametro, solo para activitys o fragments
+        CarComponent carComponent = DaggerCarComponent.create();
+        carComponent.inject(this);
+        car2.drive();
     }
 }
--- app/src/main/java/com/tutoriales/simpledagger2/components/CarComponent.java
+++ app/src/main/java/com/tutoriales/simpledagger2/components/CarComponent.java
@@ -1,5 +1,6 @@
 package com.tutoriales.simpledagger2.components;
 
+import com.tutoriales.simpledagger2.MainActivity;
 import com.tutoriales.simpledagger2.entities.Car;
 
 import dagger.Component;
@@ -7,4 +8,5 @@ import dagger.Component;
 @Component
 public interface CarComponent {
     Car getCar();
+    void inject(MainActivity activity); //se necesita declarar especificamente la activity para la forma 2
 }
